4091bbe0aef98ee618f084ead80bf1db806d1792
tom <tom@that-place>
2020-03-05 23:49:45 -0600

    Updated data model, added faster io path.

--- src/techjoin/core.clj
+++ src/techjoin/core.clj
@@ -42,8 +42,8 @@
 
 (defn random-lhs []
   (for [i (range 165000)]
-    {:gender         (rand-nth ["m" "f" "n"])
-     :address-id     (str (rand-int 100000))
+    {:size           (rand-nth ["s" "m" "l"])
+     :day            (str (rand-int 100000))
      :operator-id    (str (rand-int 10000))}))
 
 (defn random-rhs []
@@ -53,7 +53,7 @@
         (assoc c :operator-id (rand-int 10000))))))
 
 (defn spit-tables []
-  (tbl/records->file (random-lhs) "lhs.csv" :sep "," :field-order [:gender :address-id :operator-id])
+  (tbl/records->file (random-lhs) "lhs.csv" :sep "," :field-order [:size :day :operator-id])
   (tbl/records->file (random-rhs) "rhs.csv" :sep "," :field-order rhs-fields))
 
 ;; rhs - 180,000 rows -
@@ -78,10 +78,35 @@
 ;;If you've already generated your stuff...
 ;;we have files locally we can read.
 
+;;fast
 (def lhs (ds/->dataset "lhs.csv"))
+;;fast
 (def rhs (ds/->dataset "rhs.csv"))
 
+
+;;this is fast and handled quickly (makes sense since I think we are computing
+;;the join solely in index space)
 (def res (ds/join-by-column "operator-id" lhs rhs))
+
+;;slow (~90s on my hardware)
+;;I think we are realizing the joined rows as we go,
+;;and we are creating a lot of intermediate vectors and doing
+;;a lot of hashmap lookups in data.csv (according to profiling).
+;;I'm guessing these aren't necessary.
 (io/mapseq->csv! "file://test.tsv"
                  (ds/mapseq-reader res)
                  :separator \tab)
+
+;;reducer-based record traversal is ~4-5x faster than
+;;default data.csv based variant above.
+(tbl/records->file (ds/mapseq-reader res)  "test.tsv" :sep \tab))
+
+(comment
+  ;;curious to see how this works out.
+  (def lhs (tbl/tabdelimited->table "lhs.csv"))
+  (def rhs (tbl/tabdelimited->table "rhs.csv"))
+  ;;legacy implementation will blow up on this dataset,
+  ;;we inefficiently build the whole thing, and do it using
+  ;;`merge` which is slow and allocation heavy.  Punishes the heap, don't run.
+  (def res (tbl/join-on [:operator-id] lhs rhs))
+  )
